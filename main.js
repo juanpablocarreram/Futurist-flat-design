94d117c159f2b91477da5b5fa67b6df635a8284c
Juan Pablo Carrera <140585461+juanpablocarreram@users.noreply.github.com>
2024-03-14 23:28:18 -0600

    Feat: add button to link another content in page

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function moveBackground(){
     let scrollY = window.scrollY;
     introBackground.style.backgroundPositionY= 100 + scrollY * .6 + "px";
     secondSpan[1].style.transform = "translate(" + scrollY*.8 + "px,"+ scrollY*.8 + "px" ;
-    secondSpan[0].style.transform = "translate(" + -scrollY*.8 + "px," + scrollY*.8 + "px";
+    secondSpan[0].style.transform = "translate(" + -scrollY*.8  + "px," + scrollY*.8 + "px";
     firstSpan[1].style.transform = "translate(" + scrollY*.3 + "px," + scrollY*.8 + "px";
     firstSpan[0].style.transform = "translate(" + -scrollY*.3 + "px," + scrollY*.8 + "px";
 }
